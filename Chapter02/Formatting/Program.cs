d58af516e349434ccc1466621b91278b624a5e96
Eugene <eugene.ivanenko@gmail.com>
2020-02-23 11:28:34 +0200

    The first commit

---
+++ Chapter02/Formatting/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+
+namespace Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfApples = 12;
+            decimal pricePerApple = 0.35M;
+            Console.WriteLine(
+                format: "{0} apples cost {1:C}",
+                arg0: numberOfApples,
+                arg1: pricePerApple * numberOfApples
+            );
+
+            Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+
+            string applesText = "Apples";
+            int applesCount = 1234;
+            string bananasText = "Bananas";
+            int bananasCount = 56789;
+            Console.WriteLine(
+                format: "{0,-8} {1,10:D6}",
+                arg0: "Name",
+                arg1: "Count");
+            Console.WriteLine(
+                format: "{0,-8} {1,10:D6}",
+                arg0: applesText,
+                arg1: applesCount);
+            Console.WriteLine(
+                format: "{0,-8} {1,10:D6}",
+                arg0: bananasText,
+                arg1: bananasCount);
+            
+            decimal cur = 23.59M;
+            Console.WriteLine(
+                format: "{0,-8} {1,10:C4}",
+                arg0: "Decimal",
+                arg1: cur);
+
+            /* Write("Press any key combination: ");
+            ConsoleKeyInfo key = ReadKey();
+            WriteLine();
+            WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+                arg0: key.Key,
+                arg1: key.KeyChar,
+                arg2: key.Modifiers); */
+
+            WriteLine($"There are {args.Length} args");            
+            foreach (var arg in args)
+            {
+                WriteLine(arg);
+            }
+            
+        }
+    }
+}
---
+++ Chapter02/NullHandling/Program.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+using System;
+
+namespace NullHandling
+{
+    class Address{
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int thisCannotBeNull = 4;
+            // thisCannotBeNull = null;
+
+            int? thisCouldBeNull = null;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+            Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
+
+            /* if (thisCouldBeNull != null)
+            {
+                int length = thisCouldBeNull.Length;
+            } */
+
+            string authorName = null;
+            // the following throws NullReferenceException
+            //int x = authorName.Length;
+            // instead of throwing an exception, null is assigned to y
+            int? y = authorName?.Length;
+
+            var result = authorName?.Length ?? 3;
+            Console.WriteLine(result);
+
+            
+
+        }
+    }
+}
