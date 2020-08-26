6148af2592714cc3d39726a5472628f50d1efc38
Eugene <eugene.ivanenko@gmail.com>
2020-08-26 21:04:15 +0300

    Chapter10 was added

---
+++ Chapter10/CryptographyLib/User.cs
@@ -0,0 +1,11 @@
+namespace Packt.Shared
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Salt { get; set; }
+        public string SaltedHashedPassword { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
